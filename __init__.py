a29fc45888f479d57f36e6b6a986230c2e75bd50
Hanzala Zahid <hanzalarushnan@gmail.com>
2021-07-16 00:16:56 +0600

    Added manifest

--- __init__.py
+++ __init__.py
@@ -1,18 +1,23 @@
 from mycroft import MycroftSkill, intent_file_handler
 from mycroft.messagebus import Message
 from datetime import datetime
+from time import sleep
 
 class MorningBriefing(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        self.actions = []
+        self.userTitle = ''
     
     def initialize(self):
         self.userTitle = self.settings.get('userTitle', "sir")
+        self.actions = self.settings.get('actions').split(';')
         self.settings_change_callback = self.on_settings_changed
         self.on_settings_changed()
 
     def on_settings_changed(self):
         self.userTitle = self.settings.get('userTitle', "sir")
+        self.actions = self.settings.get('actions').split(';')
 
     
     @intent_file_handler('good.morning.intent')
@@ -23,9 +28,16 @@ class MorningBriefing(MycroftSkill):
                 self.speak_dialog('good.morning', dict(title=self.userTitle))
             else:
                 self.speak_dialog('not.morning', dict(title=self.userTitle))
-        self.bus.emit(Message("recognizer_loop:utterance",  
-                          {'utterances': self.settings.get('actions').split(';'),  
-                            'lang': 'en-us'}))
+        for action in self.actions:
+            self.log.info(action)
+            if 'schedule' in action:
+                self.bus.emit(Message("speak",  
+                                {'utterance': 'Your schedule for today is',  
+                                    'lang': 'en-us'}))
+            self.bus.emit(Message("recognizer_loop:utterance",  
+                            {'utterances': [action],  
+                                'lang': 'en-us'}))
+            sleep(7)
 
 
 def create_skill():
