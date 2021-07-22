e91d7b7dd66a5eef991e865333063318441e1817
Hanzala Zahid <hanzalarushnan@gmail.com>
2021-07-22 23:35:11 +0600

    update

--- __init__.py
+++ __init__.py
@@ -8,16 +8,17 @@ class MorningBriefing(MycroftSkill):
         MycroftSkill.__init__(self)
         self.actions = []
         self.userTitle = ''
+
     
     def initialize(self):
         self.userTitle = self.settings.get('userTitle', "sir")
-        self.actions = self.settings.get('actions').split(';')
+        #self.actions = self.settings.get('actions').split(';')
         self.settings_change_callback = self.on_settings_changed
         self.on_settings_changed()
 
     def on_settings_changed(self):
         self.userTitle = self.settings.get('userTitle', "sir")
-        self.actions = self.settings.get('actions').split(';')
+        self.actions = []
 
     
     @intent_file_handler('good.morning.intent')
@@ -28,16 +29,23 @@ class MorningBriefing(MycroftSkill):
                 self.speak_dialog('good.morning', dict(title=self.userTitle))
             else:
                 self.speak_dialog('not.morning', dict(title=self.userTitle))
-        for action in self.actions:
-            if 'schedule' in action:
-                self.bus.emit(Message("speak",  
-                                {'utterance': 'Your schedule for today is',  
-                                    'lang': 'en-us'}))
-            self.bus.emit(Message("recognizer_loop:utterance",  
-                            {'utterances': [action],  
-                                'lang': 'en-us'}))
-            sleep(7)
+        
+        #self.bus.emit(Message("recognizer_loop:utterance",  
+        #                    {'utterances': ['start routine shokal'],  
+        #                        'lang': 'en-us'}))
 
+        sleep(1)
+        self.bus.emit(Message("recognizer_loop:utterance",  
+                            {'utterances': ['what is the weather'],  
+                                'lang': 'en-us'}))
+        sleep(10)
+        self.bus.emit(Message("speak",  
+                            {'utterance': 'your schedule for today is',  
+                                'lang': 'en-us'}))
+        sleep(5)
+        self.bus.emit(Message("recognizer_loop:utterance",  
+                            {'utterances': ['what is my schedule for today'],  
+                                'lang': 'en-us'}))
 
 def create_skill():
     return MorningBriefing()
